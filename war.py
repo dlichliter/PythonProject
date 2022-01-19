ff2b9a85fac844a9541d2662826a7c14cb5b8a61
David Lichliter <davidlichliter1999@gmail.com>
2022-01-18 20:58:24 -0700

    - Create the game class, and initialize it upon starting the app to run the game

--- war.py
+++ war.py
@@ -4,10 +4,10 @@ from random import shuffle
 # This class represents a card in the game, with a card object having a suit and a value instance variables
 class Card:
     # Arranged by strength of suit, the strongest suit is last
-    suits = ("spades",
-             "hearts",
-             "diamonds",
-             "clubs")
+    suits = ("Spades",
+             "Hearts",
+             "Diamonds",
+             "Clubs")
 
     # First two values are none so their values match their index
     values = (None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10",
@@ -62,3 +62,70 @@ class Deck:
         if len(self.cards) == 0:
             return
         return self.cards.pop()
+
+
+# This class keeps track of a players cards and number of rounds won
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
+
+
+# This class runs the game of war between two players
+class Game:
+    # Create the players off of user input and create a deck for the game
+    def __init__(self):
+        player1 = input("Player 1 name: ")
+        player2 = input("Player 2 name: ")
+        self.deck = Deck()
+        self.p1 = Player(player1)
+        self.p2 = Player(player2)
+
+    def wins(self, winner):
+        w = "{} wins this round"
+        w = w.format(winner)
+        print(w)
+
+    # Simulate the draw of a card for both players
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} drew {} {} drew {}"
+        d = d.format(p1n, p1c, p2n, p2c)
+        print(d)
+
+    # Run the game as long as there are at least two cards left or the player does not quit the program
+    def play_game(self):
+        cards = self.deck.cards
+        print("Start of War!")
+        while len(cards) >= 2:
+            m = "q to quit. Any " + "key to play:"
+            response = input(m)
+            if response == "q":
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+
+        print("War is over. {} wins".format(win))
+
+    # Determine the winner when the deck is empty
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p1.wins < p2.wins:
+            return p2.name
+        return "It was a tie!"
+
+
+game = Game()
+game.play_game()
